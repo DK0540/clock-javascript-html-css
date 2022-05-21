ac6af691246b244bf307e3d5f2af16b69ec05bb0
Tutorial-git <dk0540@digikull.com>
2022-05-21 20:09:28 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,49 @@
+
+//  const secondHand = document.querySelector(".second-hand")
+//      const minutesHand = document.querySelector(".min-hand")
+//      const hourHand = document.querySelector(".hour-hand")
+
+
+//    function setDate() {
+//      const now = new Date();
+//      const seconds = now.getSeconds();
+//      const secondsDegrees = ((seconds/60)*360) + 90;
+//      secondHand.style.transform = `rotate(${secondsDegrees}deg)`
+
+//      const mins = now.getMinutes();
+//      const minutesDegrees = ((mins/60)*360) + 90;
+//      minutesHand.style.transform = `rotate(${minutesDegrees}deg)`
+
+//      const houre = now.getHours();
+//      const hourDegrees = ((houre/12)*360) + 90;
+//      hourHand.style.transform = `rotate(${hourDegrees}deg)`
+
+//      console.log(now);
+//    }
+//    setInterval(setDate,1000);
+
+
+// //prototype Inheritance==>
+
+// this.finctionName = function(){
+
+// }
+
+// function thisName (){
+//     this.name="javascript";
+//     this.newName=function(){
+//         console.log(this.name)
+//     }
+// }
+
+// var example1 = new thisName();
+// example1.newName();
+
+
+// var objectName = {}
+// var testArray= [1,2,3,4,"vish","boss"];
+
+// objectName.newArray=testArray;
+// objectName.title="Food on fire";
+// objectName.city="banagalore";
+// console.log(objectName)
